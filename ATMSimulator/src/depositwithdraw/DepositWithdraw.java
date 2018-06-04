46e55aea31ed4140bcfa15c77c84c3eb357a6e8c
Fiqhy Bismadhika <fiqhy.bismadhika@binus.ac.id>
2018-06-04 21:08:03 +0700

    Initial Commit
    Main program

---
+++ ATMSimulator/src/depositwithdraw/DepositWithdraw.java
@@ -0,0 +1,13 @@
+package depositwithdraw;
+
+/**
+ *
+ * @author DHIKA
+ */
+public class DepositWithdraw {
+    public static void main(String[] args){
+        LoginForm Login = new LoginForm();
+        Login.setVisible(true);
+        Login.setLocationRelativeTo(null);
+    }
+}
